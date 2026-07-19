8671bc27f3046a7dc073383e4255883ae44f1b4c
Lex Feathers <38232476+lexfeathers@users.noreply.github.com>
2026-07-18 20:24:39 -0400

    Next pass

--- _config.ts
+++ _config.ts
@@ -1,30 +1,58 @@
-import lume from "lume/mod.ts";
-import icons from "lume/plugins/icons.ts";
-import esbuild from "lume/plugins/esbuild.ts";
-import slugify_urls from "lume/plugins/slugify_urls.ts";
-import favicon from "lume/plugins/favicon.ts";
-import metas from "lume/plugins/metas.ts";
-import feed from "lume/plugins/feed.ts";
-import sitemap from "lume/plugins/sitemap.ts";
-import date from "lume/plugins/date.ts";
-import extractOrder from "lume/plugins/extract_order.ts";
+import lume from 'lume/mod.ts';
+import icons from 'lume/plugins/icons.ts';
+import esbuild from 'lume/plugins/esbuild.ts';
+import slugify_urls from 'lume/plugins/slugify_urls.ts';
+import favicon from 'lume/plugins/favicon.ts';
+import metas from 'lume/plugins/metas.ts';
+import feed from 'lume/plugins/feed.ts';
+import sitemap from 'lume/plugins/sitemap.ts';
+import date from 'lume/plugins/date.ts';
+import extractOrder from 'lume/plugins/extract_order.ts';
 
 const site = lume({
-    src: "./src",
+	src: './src',
 });
 
-site.use(icons());
+site.use(
+	icons({
+		folder: '/assets/icons',
+	})
+);
 site.use(esbuild());
 site.use(slugify_urls());
-site.use(favicon({
-    input: "/assets/icons/Mask_White.svg",
-}));
+site.use(
+	favicon({
+		input: '/assets/icons/Mask_White.svg',
+	})
+);
 site.use(metas());
-site.use(feed());
+site.use(
+	feed({
+		output: '/feed.rss',
+		query: 'type=post',
+		sort: 'date=desc',
+		info: {
+			title: '=site.title',
+			description: '=site.description',
+			lang: 'en',
+			generator: true,
+			authorName: 'Waxlimbs',
+			authorUrl: 'https://waxlimbs.com',
+		},
+		items: {
+			title: '=title',
+			description: '=excerpt',
+			published: '=date',
+			image: '=cover',
+			content: '=children',
+			authorName: '=author',
+		},
+	})
+);
 site.use(sitemap());
 site.use(date());
 site.use(extractOrder());
 
-site.add("/assets");
+site.add('/assets');
 
 export default site;
--- src/assets/scripts/global.ts
+++ src/assets/scripts/global.ts
@@ -1,5 +1,16 @@
-document.querySelectorAll("a").forEach((link: HTMLAnchorElement) => {
-    if (link.hostname != globalThis.location.hostname) {
-        link.classList.add("external")
-    }
-})
+markExternalLinks();
+
+function markExternalLinks() {
+	document.querySelectorAll('a').forEach((link: HTMLAnchorElement) => {
+		if (link.hostname != globalThis.location.hostname) {
+			link.classList.add('external');
+		}
+	});
+}
+
+// Mobile menu handling
+const hamburgerIcon = document.getElementById('hamburger');
+document.getElementById('hamburger')?.addEventListener('click', () => {
+	document.getElementById('main-menu')?.classList.toggle('expanded');
+	hamburgerIcon?.classList.toggle('icon-close');
+});
