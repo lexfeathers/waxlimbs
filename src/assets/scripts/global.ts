880903d6ff75f9b1b9282d29223c95d9d9d569ec
Lex Feathers <38232476+lexfeathers@users.noreply.github.com>
2026-07-19 00:57:03 -0400

    Next... pass....

--- src/assets/scripts/global.ts
+++ src/assets/scripts/global.ts
@@ -14,3 +14,33 @@ document.getElementById('hamburger')?.addEventListener('click', () => {
 	document.getElementById('main-menu')?.classList.toggle('expanded');
 	hamburgerIcon?.classList.toggle('icon-close');
 });
+
+// Socials
+const socials = {
+	iconInstagram: document.querySelectorAll('.Instagram'),
+	iconTiktok: document.querySelectorAll('.Tiktok'),
+	iconYoutube: document.querySelectorAll('.YouTube'),
+	iconBsky: document.querySelectorAll('.Bsky'),
+	iconMastodon: document.querySelectorAll('.Mastodon'),
+	iconBandcamp: document.querySelectorAll('.Bandcamp'),
+};
+
+// Set social icons
+socials.iconInstagram.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-instagram)`;
+});
+socials.iconTiktok.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-tiktok)`;
+});
+socials.iconYoutube.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-youtube)`;
+});
+socials.iconBsky.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-bsky)`;
+});
+socials.iconMastodon.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-mastodon)`;
+});
+socials.iconBandcamp.forEach((icon: Element) => {
+	(icon as HTMLDivElement).style.mask = `var(--icon-bandcamp)`;
+});
