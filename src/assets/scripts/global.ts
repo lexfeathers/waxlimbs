ea7c866a77b2fdfed11bc2f29317681240c6531a
Lex Feathers <38232476+lexfeathers@users.noreply.github.com>
2026-07-20 00:47:20 -0400

    Update colophon, better index image, make external links open in new tab

--- src/assets/scripts/global.ts
+++ src/assets/scripts/global.ts
@@ -6,6 +6,7 @@ function markExternalLinks() {
 	document.querySelectorAll('a').forEach((link: HTMLAnchorElement) => {
 		if (link.hostname != globalThis.location.hostname) {
 			link.classList.add('external');
+			link.target = '_blank';
 		}
 	});
 }
